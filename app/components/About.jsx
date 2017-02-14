42b0d758425287cb461b6b46b377b15930df4760
Matthew Higley <matthewhigley@MattyBook-Pro.local>
2017-02-13 20:13:32 -0800

    styleabout page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,11 +1,18 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var About = React.createClass({
     render: function(){
         return (
             <div>
-                <h3>About</h3>
-                <p>Welcome to the About page!</p>
+                <h1 className="text-center">About</h1>
+                <p>Here is information that I've learned.</p>
+                <ol>
+                    <li><a href="https://facebook.github.io/react" target="_blank">Powered by React</a></li>
+                    <li><a href="http://foundation.zurb.com/" target="_blank">Style by Foundation</a></li>
+                    <li><a href="http://openweathermap.org/" target="_blank">Weather by openWeatherMap</a></li>
+                    <li><a href="https://github.com/mhigley/reactWeather" target="_blank">My Github</a></li>
+                </ol>
             </div>
         );
     }
